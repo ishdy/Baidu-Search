d35ce9cc549a0dc35b21f40c45f15b847eaefb9a
ishdy <41664425+ishdy@users.noreply.github.com>
2026-01-30 11:50:24 +0800

    Update 百度页面样式优化-去广告，深色模式.user.js

--- 百度页面样式优化-去广告，深色模式.user.js
+++ 百度页面样式优化-去广告，深色模式.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         百度全页面样式优化-去广告，深色模式
 // @namespace    http://tampermonkey.net/
-// @version      1.57
+// @version      1.58
 // @icon         https://www.baidu.com/favicon.ico
 // @description  添加单双列布局切换，官网置顶功能，优化百度官方标识识别，增加深色模式切换，移除百度搜索结果跳转页面，并加宽搜索结果。
 // @author       Ai-Rcccccccc (Enhanced)
@@ -508,7 +508,7 @@
                        if (window.location.pathname === '/' || (window.location.pathname === '/index.php' && !window.location.search.includes('wd'))) {
                 const homepageStyles =
                       // 1. 隐藏多余元素
-                      '#form, #s_form, .s_btn_wr, .s_ipt_wr, .fm, .ai-input, .s-center-box, #s_new_search_guide, #bottom_layer, #bottom_space, #s_popup_advert, .popup-advert, .advert-shrink, .pc-dashboard, .pcsearch-common-ai-dashboard { display: none !important; }' +
+                      '#form, #s_form, .s_btn_wr, .s_ipt_wr, .fm, .ai-input, .s-center-box, #s_new_search_guide, #bottom_layer, #bottom_space, #s_popup_advert, .popup-advert, .advert-shrink { display: none !important; }' +
 
                       // 2. Logo 向上大幅度移动
                       '#lg { ' +
